84e6e71caa33dd024190cc0cbf2211a94ef220dd
min_xu <xumin_wlt@163.com>
2017-08-05 14:30:11 +0800

    first commit

---
+++ j360-sparkstream-kafka/src/main/java/me/j360/spark/kafka/Main.java
@@ -0,0 +1,117 @@
+package me.j360.spark.kafka;
+
+import kafka.serializer.StringDecoder;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import scala.Tuple2;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ * Package: me.j360.spark.kafka
+ * User: min_xu
+ * Date: 2017/8/5 下午1:12
+ * 说明：
+ */
+public class Main {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    private Main() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        directKafka(args);
+    }
+
+    public static void kafka(String[] args)  throws Exception{
+        if (args.length < 4) {
+            System.err.println("Usage: JavaKafkaWordCount <zkQuorum> <group> <topics> <numThreads>");
+            System.exit(1);
+        }
+
+        //StreamingExamples.setStreamingLogLevels();
+        SparkConf sparkConf = new SparkConf().setAppName("JavaKafkaWordCount");
+        // Create the context with 2 seconds batch size
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
+
+        int numThreads = Integer.parseInt(args[3]);
+        Map<String, Integer> topicMap = new HashMap<>();
+        String[] topics = args[2].split(",");
+        for (String topic: topics) {
+            topicMap.put(topic, numThreads);
+        }
+
+        JavaPairReceiverInputDStream<String, String> messages =
+                KafkaUtils.createStream(jssc, args[0], args[1], topicMap);
+
+        JavaDStream<String> lines = messages.map(Tuple2::_2);
+
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i1, i2) -> i1 + i2);
+
+        wordCounts.print();
+        jssc.start();
+        jssc.awaitTermination();
+    }
+
+
+    /**
+     * 需要自己去维护offset,性能更好
+     * 本次可以重复测试需要
+     * @param args
+     * @throws Exception
+     */
+    public static void directKafka(String[] args) throws Exception {
+        /*if (args.length < 2) {
+            System.err.println("Usage: JavaDirectKafkaWordCount <brokers> <topics>\n" +
+                    "  <brokers> is a list of one or more Kafka brokers\n" +
+                    "  <topics> is a list of one or more kafka topics to consume from\n\n");
+            System.exit(1);
+        }
+
+        //StreamingExamples.setStreamingLogLevels();
+
+        String brokers = args[0];
+        String topics = args[1];*/
+
+        String brokers = "localhost:9092";
+        String topics = "dmp";
+
+        // Create context with a 2 seconds batch interval
+        SparkConf sparkConf = new SparkConf().setAppName("JavaDirectKafkaWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
+
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        Map<String, String> kafkaParams = new HashMap<>();
+        kafkaParams.put("metadata.broker.list", brokers);
+
+        // Create direct kafka stream with brokers and topics
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                jssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topicsSet
+        );
+
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(Tuple2::_2);
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i1, i2) -> i1 + i2);
+        wordCounts.print();
+
+        // Start the computation
+        jssc.start();
+        jssc.awaitTermination();
+    }
+}
